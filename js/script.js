ff349c9ca55b21f437b8f6c7a482f403696d6ccd
Rostyslav <RostikL1994@gmail.com>
2021-08-15 19:49:23 +0300

    cycles

--- js/script.js
+++ js/script.js
@@ -1,19 +1,61 @@
-const numberOfFilms = +prompt ("How many movies have you watched", "");
+"use strict";
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: true
-};
+if (1) {
+    console.log('OK!');
+} else { 
+    console.log('Eror');
+}
 
-const a = prompt('What is the last movie that you have watched?', ''),
-      b = prompt('How would you mark this movie'),
-      c = prompt('What is the last movie that you have watched?', ''),
-      d = prompt('How would you mark this movie');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// if (num < 50) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('too much');
+// } else {
+//     console.log('OK!');
+// }
+// (num === 50) ? console.log('OK!') : console.log('Erorr');
 
-console.log(personalMovieDB);
+
+
+// const num = '50';
+
+// switch (num) {
+//     case '49':
+//         console.log('wrong');
+//         break;
+//     case '100':
+//         console.log('wrong');
+//         break;
+//     case '50':
+//         console.log('correct');  
+//         break;
+//     default: 
+//         console.log('not this time');
+//         break;  
+// }
+
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// do {
+    //     console.log(num);
+    //     num++;
+    // } 
+    
+    // while (num <= 55);
+    
+    let num = 50;
+
+    for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+        console.log(i);
+        
+    } 
