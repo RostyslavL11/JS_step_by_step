77801f4ab5e34001704ddc5d98d3a03f87c12e26
Rostyslav <RostikL1994@gmail.com>
2021-08-15 19:53:20 +0300

    second

--- js/script.js
+++ js/script.js
@@ -41,14 +41,6 @@ if (1) {
 //     num++;
 // }
 
-
-// do {
-    //     console.log(num);
-    //     num++;
-    // } 
-    
-    // while (num <= 55);
-    
     let num = 50;
 
     for (let i = 1; i < 10; i++) {
