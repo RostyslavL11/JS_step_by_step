82025705be78f9d4ff24a9434f57095a85133036
Rostyslav <RostikL1994@gmail.com>
2021-09-26 21:38:42 +0300

    2.20 Oбъекты, деструктуризация объектов (ES6)

--- js/script.js
+++ js/script.js
@@ -1,30 +1,58 @@
 "use strict";
-// 2.19
+// 2.20 Объекты, деструктуризация объектов (ES6)
 
-function first() {
-        // Do something
-        setTimeout(function() {
-            console.log(1);
-        }, 500);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("test");
+    }
+};
 
-function second() {
-    console.log(2);
-}
+options.makeTest();
 
-first();
+const {border, bg} = options.colors;
+console.log(border);
 
-second();
+// console.log(Object.keys(options).length);
 
-function learnJS(lang, callback) {
-    console.log(`I learn: ${lang}`);
-    callback()
-}
+// console.log(options["colors"]["border"]);
 
-function done() {
-    console.log('I nailed this lesson');
-}
+// console.log(options.name);
 
-learnJS('JavaScript', done);
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Властивість ${i} має значення ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Властивість ${key} має значення ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+// як взнати яка кількість пар ключ - значення є в середині обєкта?
 
 
+const fruts = {
+    names: "apple",
+    count: 4,
+    color: "red",
+    tasty: {
+        sweet: 3,
+        bitter: 1
+    }
+};
+
+console.log(Object.keys(fruts).length);
