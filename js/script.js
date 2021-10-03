cd1eae8ed5a78408fbaf0e0cc82d42ad0b858f45
Rostyslav <RostikL1994@gmail.com>
2021-10-03 22:06:03 +0300

    O сновы , прототипно-ориентированное наследование

--- js/script.js
+++ js/script.js
@@ -1,115 +1,51 @@
 "use strict";
-// 22. Передача по ссылке или по значению, Spread оператор (ES6-ES9)
+// 23. Основы ООП, прототипно-ориентированное наследование
 
-// let a = 5,
-//     b = a;
+let str = "some";
+let strObj = new String(str);
 
-// b = a + 5;
+// console.log(typeof(str));
+// console.log(typeof(strObj));
 
-// console.log(b);
-// console.log(a);
+console.dir([1,2,3]);
 
 
-
-// const obj = {
-//     a: 5, 
-//     b:1
+// __________________________
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log('Hello');
+//     }
 // };
 
-// const copy = obj; // посилання
-// copy.a = 10;
+// const jonh = {
+//     health: 100
+// };
 
-// console.log(copy);
-// console.log(obj);
-// ___________________________________
+// // jonh.__proto__ = soldier;
 
+// Object.setPrototypeOf(jonh, soldier);
+// // jonh обєкт якому я хочу назначити прототип
+// //  soldier обєкт прототип якого ми встановлюємо 
 
+// jonh.sayHello();
 
-// створюємо копію обєкту за допомогою функції 
-// яка за допомогою цикла вона створить новий обєкт 
-// вже перебираючи старі властивості які вже були в нашому обєкті 
+// console.log(jonh.armor);
 
-function copy(mainObj) {
-    let objCopy = {};
+// _______________________________
+// коли ми створюємо протоп до неіснуючого обєкту 
 
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7, 
-        y:4
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('Hello');
     }
 };
 
-const  newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-
-// console.log(newNumbers);
-// console.log(numbers);
-// // _____________________________
-// зєднуємо обєкт add з обєктом numbers 
-
-const add = {
-    d: 17,
-    e:20 
-};
-
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-console.log(add);
-console.log(clone);
-
-// метод щоб створили копію масиву
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'hello';
-
-console.log(newArray);
-console.log(oldArray);
-
-// 4 спосіб створення поверхневої копії з вик. оператора розвороту 
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'bloger'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-
-      console.log(internet);
-
-    
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-const array = ['a', 'b'];
-
-const narArrray = [...array];
+const jonh = Object.create(soldier);
 
 
-const q = {
-    one: 1, 
-    two: 2
-};
+jonh.sayHello();
 
-const newQ = {...q};
-
-console.log(newQ);
