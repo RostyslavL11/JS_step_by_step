3acd5b83c7bca016ec15bdb85543f26221121176
Rostyslav <RostikL1994@gmail.com>
2021-10-04 14:44:45 +0300

    24. Практика , ч4. Используем объекты

--- js/script.js
+++ js/script.js
@@ -1,51 +1,96 @@
 "use strict";
-// 23. Основы ООП, прототипно-ориентированное наследование
+// 24. Практика , ч4. Используем объекты
 
-let str = "some";
-let strObj = new String(str);
+/* Задание на урок:
 
-// console.log(typeof(str));
-// console.log(typeof(strObj));
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 
-console.dir([1,2,3]);
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
 
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
-// __________________________
-// const soldier = {
-//     health: 400,
-//     armor: 100,
-//     sayHello: function() {
-//         console.log('Hello');
-//     }
-// };
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                  b = prompt('На сколько оцените его?', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i < 2; i++) {
+            // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
 
-// const jonh = {
-//     health: 100
-// };
+            // if (genre === '' || genre === null) {
+            //     console.log('Вы ввели некорректные даные или не ввели их вовсе');
+            //     i--;
+            // } else {
+            //     personalMovieDB.genres[i - 1] = genre;
+            // }
 
-// // jonh.__proto__ = soldier;
+            // інакший спосіб запису
+            let genres = prompt(`Введите ваши любимие жанри через запятую`).toLowerCase();
 
-// Object.setPrototypeOf(jonh, soldier);
-// // jonh обєкт якому я хочу назначити прототип
-// //  soldier обєкт прототип якого ми встановлюємо 
+            if (genres === '' || genres === null) {
+                console.log('Вы ввели некорректные даные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres = genres.split(', ');
+                personalMovieDB.genres.sort();
+            }
+        }
 
-// jonh.sayHello();
-
-// console.log(jonh.armor);
-
-// _______________________________
-// коли ми створюємо протоп до неіснуючого обєкту 
-
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function() {
-        console.log('Hello');
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
     }
 };
-
-const jonh = Object.create(soldier);
-
-
-jonh.sayHello();
 
